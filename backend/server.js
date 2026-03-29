94b63508f9fd0944573ec69da4c0d531771e3757
Saswath Menon <saswath62@gmail.com>
2026-03-29 11:14:19 +0000

    Added CORS origin URLs for easier backend - frontend integration

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const driverStandingRoutes = require("./routes/driverStandingRoutes");
 const constructorStandingRoutes = require("./routes/constructorStandingRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -19,7 +20,17 @@ mongoose.connect(process.env.MONGO_URI)
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://f1-companion.vercel.app"
+  ]
+}));
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
 app.use(express.json());
 
 app.use("/assets", express.static("assets"));
@@ -31,6 +42,6 @@ app.use("/api/constructors", constructorRoutes);
 app.use("/api/driver-standings", driverStandingRoutes);
 app.use("/api/constructor-standings", constructorStandingRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
